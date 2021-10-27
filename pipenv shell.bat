e01f3b314f3a2bbcdcc08f32680a46d4ea2fc69e
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-10-27 17:25:08 -0400

    fixed popup notation in for Easting main.py

--- pipenv shell.bat
+++ pipenv shell.bat
@@ -1,11 +1,11 @@
-echo Ejecutando ambiente virtual
-echo Coloque luego: pipenv run main.py
-
 cd /d %~dp0
 @echo off
 cd %
 
 @echo on
+echo Ejecutando ambiente virtual
+echo Coloque luego: pipenv run main.py
+
 pipenv shell
 
 echo END
