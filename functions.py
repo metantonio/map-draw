c313ab2ba763f870307c79aee38b69c21b9368ef
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-10-27 09:51:27 -0400

    agregado menú, y cambiado formato de hoja data

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-def excel_GMS2UTM():
-    df_excel = pd.read_excel('data.xls', sheet_name="GMS_a_UTM")
+def excel_Localizacion():
+    df_excel = pd.read_excel('data.xls', sheet_name="LOCALIZACION")
     df_excel_columnas = df_excel.columns
     #print("Excel Heads: \n", df_excel_columnas)
     df_dataset_lenght = df_excel.iloc[0,0]
@@ -17,11 +17,55 @@ def excel_GMS2UTM():
         feature.append([df_excel.iloc[i,6],df_excel.iloc[i,11]])
         #print("\n Norte: " + str(norte_GMS[i]) + " Este: " + str(este_GMS[i]))
     data = pd.DataFrame(feature, columns=["Norte/Sur","Este/Oeste"])
-    print("\n initial data: \n", data)
+    print("\n Coordenadas de Localización: \n", data)
     #print(feature)
     #print("\n initial weights \n", weights)
     return data, norte_GMS, este_GMS, feature
-  
+
+def excel_Linea():
+    df_excel = pd.read_excel('data.xls', sheet_name="LINEA")
+    df_excel_columnas = df_excel.columns
+    #print("Excel Heads: \n", df_excel_columnas)
+    df_dataset_lenght = df_excel.iloc[0,0]
+    #print("\n Dataset lenght: ",df_dataset_lenght)
+    #variables = int(input("\n How many variables for inputs exist? (x1, x2, ...)\n"))
+    norte_GMS=[]
+    este_GMS=[]
+    feature=[]
+    for i in range(df_dataset_lenght):
+        norte_GMS.append(df_excel.iloc[i,6]) #el index en para extraer data de columnas empieza en 0, por eso es columna 6 y no 7
+        este_GMS.append(df_excel.iloc[i,11])
+        feature.append([df_excel.iloc[i,6],df_excel.iloc[i,11]])
+        #print("\n Norte: " + str(norte_GMS[i]) + " Este: " + str(este_GMS[i]))
+    data = pd.DataFrame(feature, columns=["Norte/Sur","Este/Oeste"])
+    print("\n Vértices Polilínea: \n", data)
+    #print(feature)
+    #print("\n initial weights \n", weights)
+    return data, norte_GMS, este_GMS, feature
+
+def excel_Circulo():
+    df_excel = pd.read_excel('data.xls', sheet_name="CIRCULO")
+    df_excel_columnas = df_excel.columns
+    #print("Excel Heads: \n", df_excel_columnas)
+    df_dataset_lenght = df_excel.iloc[0,0]
+    #print("\n Dataset lenght: ",df_dataset_lenght)
+    #variables = int(input("\n How many variables for inputs exist? (x1, x2, ...)\n"))
+    norte_GMS=[]
+    este_GMS=[]
+    radio=[]
+    feature=[]
+    for i in range(df_dataset_lenght):
+        norte_GMS.append(df_excel.iloc[i,6]) #el index en para extraer data de columnas empieza en 0, por eso es columna 6 y no 7
+        este_GMS.append(df_excel.iloc[i,11])
+        radio.append(df_excel.iloc[i,12])
+        feature.append([df_excel.iloc[i,6],df_excel.iloc[i,11]])
+        #print("\n Norte: " + str(norte_GMS[i]) + " Este: " + str(este_GMS[i]))
+    data = pd.DataFrame(feature, columns=["Norte/Sur","Este/Oeste"])
+    print("\n Coordenadas Círculo: \n", data)
+    #print(feature)
+    #print("\n initial weights \n", weights)
+    return data, norte_GMS, este_GMS, feature, radio
+ 
 def save_csv(data, weights, bias, l_rate, epochs, epoch_loss, loss, average_loss):
     print("\n Weights: \n",weights)
     df= pd.DataFrame({'Weights':weights, 'Epoch':epochs})
--- main.py
+++ main.py
@@ -5,29 +5,107 @@ import folium
 from folium import plugins
 
 #import of functions.py
-from functions import excel_GMS2UTM, save_csv
+from functions import excel_Localizacion, save_csv, excel_Linea, excel_Circulo
 
-data, norte_GMS, este_GMS, coordenadas = excel_GMS2UTM()
-#print(norte_GMS, este_GMS)
+#Agregando opción de mostrar coordenadas por donde va pasando el mouse
+def formatoMouse(my_map):
+    formatter = "function(num) {return L.Util.formatNum(num, 3) + ' º ';};"
+    return plugins.MousePosition(
+        position='topright',
+        separator=' | ',
+        empty_string='NaN',
+        lng_first=True,
+        num_digits=20,
+        prefix='Coordinates:',
+        lat_formatter=formatter,
+        lng_formatter=formatter,
+        ).add_to(my_map)
 
-#Creando Mapa
-myMap = folium.Map(location = coordenadas[0], zoom_start = 9, tiles='Stamen Terrain')
+#definicion de la grilla o grid:
+def grilla():
+    lat_interval = 1
+    lon_interval = 1
 
-#Agregando marcas de posición a las coordenadas
-for i in range(len(coordenadas)):
-    folium.Marker(coordenadas[i],popup = (str(i)+"\n N:"+str(coordenadas[i][0])+"\n S:"+str(coordenadas[i][1]))).add_to((myMap))
+    for lat in range(-90, 91, lat_interval):
+        folium.PolyLine([[lat, -180],[lat, 180]], weight=0.5).add_to(myMap)
 
-#Agregando líneas entre coordenadas
-folium.PolyLine(coordenadas, color="red", weight=2.5, opacity=1).add_to(myMap)
+    for lon in range(-180, 181, lon_interval):
+        folium.PolyLine([[-90, lon],[90, lon]], weight=0.5).add_to(myMap) 
 
-#Agregar Cículos
-#folium.Circle([latitude5, longitude5], radius=9000, popup = (str(i)+"\n N:"+str(coordenadas[i][0])+"\n S:"+str(coordenadas[i][1])), line_color='#3186cc',fill_color='#3186cc', fill=True).add_to((myMap))
+#Menú de Bienvenida
+print("""\n Bienvenidos al Script mapa-draw.
+
+Menú:
+
+1-. Dibujar Puntos de Localización
+2-. Dibujar Polilínea
+3-. Dibujar Círculos
+4-. Dibujar Puntos de Localizacion, Polilíneas y Círculos al mismo tiempo
+5-. Salir
+""")
+
+user=int(input("\n Elige una opción \n"))
+
+if (user==1):    
+    data, norte_GMS, este_GMS, coordenadas = excel_Localizacion()
+    #print(norte_GMS, este_GMS)
+
+    #Creando Mapa
+    myMap = folium.Map(location = coordenadas[0], zoom_start = 9, tiles='Stamen Terrain')
+
+    #Agregando marcas de posición a las coordenadas
+    for i in range(len(coordenadas)):
+        folium.Marker(coordenadas[i],popup = (str(i)+"\n N:"+str(coordenadas[i][0])+"\n S:"+str(coordenadas[i][1]))).add_to((myMap))
+
+    
+    
+if (user==2):    
+    data, norte_GMS, este_GMS, coordenadasL = excel_Linea()
+    #print(norte_GMS, este_GMS)
+
+    #Creando Mapa
+    myMap = folium.Map(location = coordenadasL[0], zoom_start = 9, tiles='Stamen Terrain')
+
+    #Agregando líneas entre coordenadas
+    folium.PolyLine(coordenadasL, color="red", weight=2.5, opacity=1).add_to(myMap)
+
+if (user==3):    
+    data, norte_GMS, este_GMS, coordenadasC, radio = excel_Circulo()
+    #print(norte_GMS, este_GMS)
+
+    #Creando Mapa
+    myMap = folium.Map(location = coordenadasC[0], zoom_start = 9, tiles='Stamen Terrain')
+
+    #Agregando círculos en las coordenadas
+    for i in range(len(coordenadasC)):
+        folium.Circle(coordenadasC[i], radius=radio[i], popup = (str(i)+"\n Centro es: \n N:"+str(coordenadasC[i][0])+"\n S:"+str(coordenadasC[i][1])+"\n Radio(m):"+str(radio[i])), line_color='#3186cc',fill_color='#3186cc', fill=True).add_to((myMap))
+
+if(user==4):
+    data, norte_GMS, este_GMS, coordenadas = excel_Localizacion()
+    data, norte_GMS, este_GMS, coordenadasL = excel_Linea()
+    data, norte_GMS, este_GMS, coordenadasC, radio = excel_Circulo()
+    #print(norte_GMS, este_GMS)
+
+    #Creando Mapa
+    myMap = folium.Map(location = coordenadas[0], zoom_start = 9, tiles='Stamen Terrain')
+
+    #Agregando marcas de posición a las coordenadas
+    for i in range(len(coordenadas)):
+        folium.Marker(coordenadas[i],popup = (str(i)+"\n N:"+str(coordenadas[i][0])+"\n S:"+str(coordenadas[i][1]))).add_to((myMap))
+
+    #Agregando líneas entre coordenadas
+    folium.PolyLine(coordenadasL, color="red", weight=2.5, opacity=1).add_to(myMap)
+
+    #Agregando círculos en las coordenadas
+    for i in range(len(coordenadasC)):
+        folium.Circle(coordenadasC[i], radius=radio[i], popup = (str(i)+"\n Centro es: \n N:"+str(coordenadasC[i][0])+"\n S:"+str(coordenadasC[i][1])+"\n Radio(m):"+str(radio[i])), line_color='#3186cc',fill_color='#3186cc', fill=True).add_to((myMap))
+
 
 #Agregar Rectángulos (con tuplas desde esquina inferior izq a esquina superior derecha)
 #folium.Rectangle(bounds=[(37.554, 126.95), (37.556, 126.97)],fill=True,color='orange', tooltip='this is Rectangle').add_to(korea)
 
 #Distintas opciones de visualizacion del mapa
-folium.TileLayer('openstreetmap').add_to(myMap)
+#folium.TileLayer('openstreetmap').add_to(myMap)
 #folium.TileLayer('mapquestopen').add_to(myMap)
 #folium.TileLayer('MapQuest Open Aerial').add_to(myMap)
 #folium.TileLayer('Mapbox Bright').add_to(myMap)
@@ -35,38 +113,24 @@ folium.TileLayer('openstreetmap').add_to(myMap)
 #folium.TileLayer('stamenterrain').add_to(myMap)
 #folium.TileLayer('stamentoner').add_to(myMap)	
 #folium.TileLayer('cartodbpositron').add_to(myMap)
+#folium.LayerControl().add_to(myMap)
+folium.TileLayer('openstreetmap').add_to(myMap)
 folium.LayerControl().add_to(myMap)
 
-#Agregando opción de mostrar coordenadas por donde va pasando el mouse
-formatter = "function(num) {return L.Util.formatNum(num, 3) + ' º ';};"
-plugins.MousePosition(
-    position='topright',
-    separator=' | ',
-    empty_string='NaN',
-    lng_first=True,
-    num_digits=20,
-    prefix='Coordinates:',
-    lat_formatter=formatter,
-    lng_formatter=formatter,
-).add_to(myMap)
-
 #Agregando la grilla con cada 1 grados de diferencia
-lat_interval = 1
-lon_interval = 1
-
-for lat in range(-90, 91, lat_interval):
-     folium.PolyLine([[lat, -180],[lat, 180]], weight=0.5).add_to(myMap)
-
-for lon in range(-180, 181, lon_interval):
-    folium.PolyLine([[-90, lon],[90, lon]], weight=0.5).add_to(myMap)
+grilla()
 
 #Exportar Mapa
-draw = plugins.Draw(export=True)
-draw.add_to(myMap)
+#draw = plugins.Draw(export=True)
+#draw.add_to(myMap)
+    
+
+formatoMouse(myMap)
 
 #Generando mapa
 myMap.save('Mapa.html')
-print("mapa creado como: Mapa.html")
+print("\n mapa creado como: Mapa.html")
+
     
 #example=[]
 #df_excel = pd.read_excel('data.xls', sheet_name='evaluation')
