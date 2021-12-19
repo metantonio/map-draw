64c41d336eab4456e1ea383cc33cbd971d06f4e1
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-12-19 11:37:43 -0400

    updated

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def menuPpal(user):
             #HeatMap(heatMapData, name="Radiacion Externa", gradient={0.0: 'pink', 0.15: 'blue', 0.3: 'green',  0.7: 'yellow', 1: 'red'}, blur=5, radius=25, min_opacity=0.0).add_to(myMap)
             #HeatMap([[norte_GMSP[0], este_GMSP[0], 1]], name="Radiacion Centro", gradient={0.0: 'pink', 0.15: 'blue', 0.3: 'green',  0.7: 'yellow', 1: 'red'}, blur=1, radius=30, min_opacity=1.0).add_to(myMap)
             #HeatMap(dataHeatMap, name="Radiacion Interna", gradient={0.0: 'pink', 0.15: 'blue', 0.3: 'green',  0.7: 'yellow', 1: 'red'}, blur=1, radius=35, max_zoom=25).add_to(myMap)
-            HeatMapWithTime([dataHeatMap], name="Radiacion E.M.", gradient={0.0: 'pink', 0.2: 'blue', 0.4: 'green',  0.7: 'yellow', 1: 'red'}, radius=50, display_index=False, min_opacity=0, max_opacity=1.0, position='topright').add_to(myMap)
+            HeatMapWithTime([dataHeatMap], name="Radiacion E.M.", gradient={0.0: 'pink', 0.2: 'blue', 0.4: 'green',  0.7: 'yellow', 1: 'red'}, radius=50, display_index=False, min_opacity=0.4, max_opacity=0.6, position='topright').add_to(myMap)
             
         if(desicion==2):
             for element in dataFinal:
